df924cf2a0512f6fccfda87608833625f109a7a4
Alina Chernyakevich <87317533+aachernyakevich-sc@users.noreply.github.com>
2023-09-19 19:42:23 +0300

    Added opportunity to change config (#12)
    Added opportunity to use browser/user specific configuration for the script.

--- low-checker.user.js
+++ low-checker.user.js
@@ -7,7 +7,12 @@
 // @match        http://localhost:8080/*
 // @match        https://*.phoebius.com/issues/*
 // @grant        GM_log
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
+// @grant        GM_listValues
 // @grant        GM_registerMenuCommand
+// @require      https://bitbucket.org/achernyakevich/tmsp-common/raw/configHelper-0.1.3/configHelper.js
 // ==/UserScript==
 
 (function() {
@@ -51,29 +56,36 @@
         })
     }
 
-    const extractAndValidate = () => {
-        validate(document.getElementById(config.targetElementId).value);
+    const extractAndValidate = (targetElementId) => () => {
+        validate(document.getElementById(targetElementId).value);
     }
 
-    const CONFIGURATIONS = [
-        {
-            urlPattern: "http:\\/\\/localhost:",
-            targetElementId: "text"
-        },
-        {
-            urlPattern: "^https:\\/\\/.+\\.phoebius.com\\/issues\\/\\d+",
-            targetElementId: "issue_description"
-        }
-    ];
+    const CONFIG_NAMESPACE = "low-checker";
+    const DEFAULT_CONFIG = JSON.stringify({
+        targetElementMatchers: [
+            {
+                urlPattern: "http:\\/\\/localhost:",
+                targetElementId: "text"
+            },
+            {
+                urlPattern: "^https:\\/\\/.+\\.phoebius.com\\/issues\\/\\d+",
+                targetElementId: "issue_description"
+            }
+        ],
+        blackListValidatorDictionary: []
+    });
 
-    const config = CONFIGURATIONS.find((el) => new RegExp(el.urlPattern).test(window.location.href));
+    configHelper.addConfigMenu(CONFIG_NAMESPACE, DEFAULT_CONFIG);
+    const config = configHelper.getConfig(CONFIG_NAMESPACE);
+    const matcher = config.targetElementMatchers.find((el) => new RegExp(el.urlPattern).test(window.location.href));
+    const targetElementId = matcher.targetElementId;
 
-    if (config) {
+    if (matcher) {
         document.addEventListener('keydown', function(event) {
             //GM_log("Ctrl: " + event.ctrlKey +"; Shift: " + event.shiftKey + "; Key: " + event.key + "; Code: " + event.code);
 
             if ( event.altKey && event.shiftKey && event.code == 'KeyC') {
-                extractAndValidate();
+                extractAndValidate(targetElementId)();
                 event.stopPropagation();
                 event.preventDefault();
             }
@@ -81,7 +93,7 @@
 
         GM_log("Shortcuts assigned");
 
-        GM_registerMenuCommand("Check List of Works", extractAndValidate, 'c');
+        GM_registerMenuCommand("Check List of Works", extractAndValidate(targetElementId), 'c');
     } else {
         GM_log("LoW Checker: Configuration not found.");
     }
