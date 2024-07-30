7381cb551d5c1486fe1d83e5eef06568bf5c2fb2
Alex Chernyakevich <tch@scand.com>
2024-07-30 22:01:51 +0300

    TrackerPeriodUpdater: added support of in..ces

--- tracker-period-updater.user.js
+++ tracker-period-updater.user.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name         TrackerPeriodUpdater
 // @namespace    https://github.com/achernyakevich-sc/dl-toolset-pub/
-// @version      0.1.0
+// @version      0.1.1
 // @description  This script brings possibility to move forward period of the tracker.
 // @author       Alexander Chernyakevich <tch@scand.com>
-// @include      /^https:\/\/.+\.phoebius\.com\/issues\/\d+/
-// @match        https://*.phoebius.com/*/issues/*/copy
+// @include      /^https:\/\/.+\.ph.+us\.com\/(.+\/)*issues\/\d+(\/copy)*/
 // @grant        GM_log
 // @grant        GM_registerMenuCommand
 // ==/UserScript==
@@ -13,12 +12,12 @@
 (function() {
     'use strict';
 
-    const updatePeriod = () => {
+    const updateForm = () => {
         let subjectInput = document.getElementById("issue_subject");
         let descriptionTextarea = document.getElementById("issue_description");
         let statusSelect = document.getElementById("issue_status_id");
         let assigneeSelect = document.getElementById("issue_assigned_to_id");
-        let periodInput = document.getElementById("issue_custom_field_values_18");
+        let periodInput = document.getElementById(periodInputId);
         let linkIssueCheckbox = document.getElementById("link_copy");
         let copyAttachmentsCheckbox = document.getElementById("copy_attachments");
 
@@ -33,6 +32,16 @@
         periodInput.value = newPeriod;
         subjectInput.value = subjectInput.value.replace(oldPeriod, newPeriod)
 
+        if ( !isReportsHost ) {
+            document.getElementById("issue_custom_field_values_5").value = ""; // Report field
+            document.getElementById("issue_custom_field_values_8").value = ""; // Issuing Date field
+            document.getElementById("issue_custom_field_values_7").value = ""; // Payment Date field
+            document.getElementById("issue_custom_field_values_17").selectedIndex = 2; // Recorded field
+
+            let numberInput = document.getElementById("issue_custom_field_values_6");
+            numberInput.value = ( numberInput.value.trim() != "" ? parseInt(numberInput.value) + 1 : "" );
+        }
+
         descriptionTextarea.value = "";
         if ( linkIssueCheckbox ) {
             linkIssueCheckbox.checked = false;
@@ -43,21 +52,23 @@
         assigneeSelect.selectedIndex = 1;
         statusSelect.selectedIndex = 0;
 
-        GM_log("Update called");
+        GM_log("Form updated.");
     }
+
+    let isReportsHost = ( document.location.hostname.indexOf("reports") == 0 );
+    let periodInputId = ( isReportsHost ? "issue_custom_field_values_18" : "issue_custom_field_values_4" );
 
     document.addEventListener("keydown", (event) => {
             // GM_log("Ctrl: " + event.ctrlKey +"; Shift: " + event.shiftKey + "; Key: " + event.key + "; Code: " + event.code);
 
             if (event.altKey && event.shiftKey && event.code == "KeyP") {
-                GM_log("pressed");
-                updatePeriod();
+                updateForm();
                 event.stopPropagation();
                 event.preventDefault();
             }
         }, true);
 
-    GM_log("Shortcuts assigned");
+    GM_log("TrackerPeriodUpdater: shortcuts assigned");
 
-    GM_registerMenuCommand("Updated period", updatePeriod, "u");
+    GM_registerMenuCommand("Updated period", updateForm, "p");
 })();
