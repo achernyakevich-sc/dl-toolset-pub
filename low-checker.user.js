4e35247bea69e40488d0cb5b660f7ad4492c7ff3
Alex Chernyakevich <tch@scand.com>
2025-03-06 18:38:19 +0300

    LoW Checker: Advancing script version after release 0.5.0

--- low-checker.user.js
+++ low-checker.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         LoW-Checker
-// @version      0.5-SNAPSHOT
+// @version      0.6-SNAPSHOT
 // @description  List of Work (LoW) Checker
 // @author       calina@scand.com
 // @author       bosak@scand.com
