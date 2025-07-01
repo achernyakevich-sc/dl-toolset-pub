cf18b9e3e0e43bbe3014c759c6555ec1f9631010
Alex Chernyakevich <tch@scand.com>
2025-07-01 14:56:28 +0300

    LoW-Checker: upgrade development version to 0.6.SNAPSHOT version.

--- low-checker.user.js
+++ low-checker.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         LoW-Checker
-// @version      0.5.1
+// @version      0.6-SNAPSHOT
 // @description  List of Work (LoW) Checker
 // @author       calina@scand.com
 // @author       bosak@scand.com
